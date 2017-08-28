564666fa1e386d15fd6672b1ad3d8b1cdc9e1c3e
chimir <zhe9191@yandex.ru>
2017-08-28 16:24:36 +0200

    Валидация поля имени

--- js/setup.js
+++ js/setup.js
@@ -185,31 +185,22 @@ fireball.addEventListener('click', function () {
   fireball.setAttribute('style', 'background: ' + fireballRandomColor);
 });
 
-// Выводим свои сообщения об ошибке ввода в поле.
-userNameInput.addEventListener('invalid', function () {
-  if (!userNameInput.validity.valid) {
-    if (userNameInput.validity.tooShort) {
-      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-    } else if (userNameInput.validity.tooLong) {
-      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
-    } else if (userNameInput.validity.valueMissing) {
-      userNameInput.setCustomValidity('Обязательное поле');
-    }
+// Валидация поля - Имя персонажа
+userNameInput.addEventListener('input', function () {
+  var minLength = userNameInput.minLength;
+
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из ' + minLength + '-х символов');
   } else {
     userNameInput.setCustomValidity('');
   }
-});
 
-// Для браузера Edge добавляем проверку на длинну заполненности поля
-// и выводим соответствующее сообщение.
-userNameInput.addEventListener('input', function (evt) {
-  var target = evt.target;
-
-  if (target.value.length < 2) {
-    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-  } else if (target.value.length > 25) {
-    target.setCustomValidity('Имя не должно превышать 25-ти символов');
+  // Для браузера Edge
+  if (userNameInput.value.length < 2) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.value.length > 25) {
+    userNameInput.setCustomValidity('Имя не должен превышать 25 символов');
   } else {
-    target.setCustomValidity('');
+    userNameInput.setCustomValidity('');
   }
 });
