4846cf50dd423180a3bae46b7b17905d23b87c19
chimir <zhe9191@yandex.ru>
2017-09-02 19:11:01 +0200

    Добавил функцию оздания DOM-элемента для вывода ошибки

--- js/util.js
+++ js/util.js
@@ -22,6 +22,26 @@
 
     getRandom: function (min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
+    },
+
+    // Сообщение об шибке.
+    errorHandler: function (errorMessage) {
+      var node = document.createElement('div');
+
+      node.style.backgroundColor = '#323232';
+      node.style.fontSize = '14px';
+      node.style.lineHeight = '20px';
+      node.style.minWidth = '288px';
+      node.style.maxWidth = '568px';
+      node.style.borderRadius = '2px';
+      node.style.padding = '14px 24px';
+      node.style.position = 'absolute';
+      node.style.left = '20px';
+      node.style.bottom = '20px';
+      node.style.zIndex = '99';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
     }
   };
 })();
